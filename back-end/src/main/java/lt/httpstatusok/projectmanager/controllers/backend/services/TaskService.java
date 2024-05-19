9bd774a1d920dd27ab9b05c98d89f5272b6c4302
Kazimieras <kazischomicenko@gmail.com>
2024-05-20 01:34:41 +0300

    back & front csv method created. Projects line break bug fixed

--- back-end/src/main/java/lt/httpstatusok/projectmanager/controllers/backend/services/TaskService.java
+++ back-end/src/main/java/lt/httpstatusok/projectmanager/controllers/backend/services/TaskService.java
@@ -1,9 +1,11 @@
 package lt.httpstatusok.projectmanager.controllers.backend.services;
 
 import lt.httpstatusok.projectmanager.controllers.backend.dto.TaskCreateRequest;
+import lt.httpstatusok.projectmanager.controllers.backend.models.Project;
 import lt.httpstatusok.projectmanager.controllers.backend.models.Task;
 import lt.httpstatusok.projectmanager.controllers.backend.models.enums.TaskStatus;
 
+import java.io.Writer;
 import java.util.List;
 
 public interface TaskService {
@@ -18,4 +20,8 @@ public interface TaskService {
     List<Task> findTasksByName(String name);
 
     List<Task> findTasksByStatus(TaskStatus status);
+
+    void writeTasksToCsv(List<Task> projects, Writer writer) throws java.io.IOException;
+
+    List<Task> getAllTasks();
 }
